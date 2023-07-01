231e024f92d0a5f9c0c9f6536c9c63a289ba6daa
nazim-musayev <nazim.musa.555@gmail.com>
2023-07-01 15:59:29 +0400

    creating repo

---
+++ App.tsx
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? '#000000' : '#FFFFFF',
+  };
+
+  return (
+    <SafeAreaView style={[backgroundStyle, styles.container]}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Text>
+        Project init
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+export default App;
