a046cd4415f49254780dc7278c520aeaef81a63c
nazim-musayev <nazim.musa.555@gmail.com>
2023-07-16 19:26:44 +0400

    changing app name

--- android/app/src/main/java/com/ari/app/MainActivity.java
+++ android/app/src/main/java/com/ari/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.chaghir;
+package com.ari.app;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Chaghir";
+    return "Arı";
   }
 
   @Override
