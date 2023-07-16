8e96abf9dbc217143ea54d47e145b12fcc0dce35
nazim-musayev <nazim.musa.555@gmail.com>
2023-07-16 16:42:50 +0400

    testing new remote url

--- src/navigation/screens/sign-in/index.tsx
+++ src/navigation/screens/sign-in/index.tsx
@@ -1,21 +1,13 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from 'react-native';
 
 function SignInScreen() {
   return (
-    <View style={styles.container}>
-      <Text>
+    <View className='flex bg-red-300'>
+      <Text className='text-yellow-600'>
         SignIn Screen
       </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default SignInScreen;
